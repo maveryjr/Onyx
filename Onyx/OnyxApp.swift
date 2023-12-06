08c6829ac08ce616ede0d6449f4fb4f65337127e
Michael Avery <42273486+maveryjr@users.noreply.github.com>
2023-12-06 11:14:14 -0500

    Initial Commit

---
+++ Onyx/OnyxApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnyxApp.swift
+//  Onyx
+//
+//  Created by Michael L. Avery Jr. on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct OnyxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
